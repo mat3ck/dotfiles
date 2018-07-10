2feb57139927b704d8b8b68e3ae2eac501bc1c04
Mathieu PILLET <math.pillet@hotmail.com>
2018-07-10 10:39:20 +0200

    Added sync script for dotfiles

---
+++ sync.sh
@@ -0,0 +1,6 @@
+# Just a little script to sync this folder with system dotfiles
+
+# zsh dotfiles
+cp -fv ./zshenv ~/.zshenv
+cp -fv ./config/zsh/zprofile ~/.config/zsh/.zprofile
+cp -fv ./config/zsh/zshrc ~/.config/zsh/.zshrc
