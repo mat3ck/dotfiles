172f1cd05e9ed0b1c47525c44d64b82d7136dfc9
Mathieu PILLET <math.pillet@hotmail.com>
2018-07-10 10:53:13 +0200

    Added termite config files and support in sync.sh

--- sync.sh
+++ sync.sh
@@ -5,5 +5,8 @@ cp -fv ./zshenv ~/.zshenv
 cp -fv ./config/zsh/zprofile ~/.config/zsh/.zprofile
 cp -fv ./config/zsh/zshrc ~/.config/zsh/.zshrc
 
+# termite
+cp -frv ./config/termite ~/.config
+
 # sway
-cp -fv ./config/sway/* ~/.config/sway/
+cp -frv ./config/sway ~/.config
