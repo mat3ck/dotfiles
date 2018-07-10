0451b1ca160d8f3f2435dfc7ec53e1e4b1573faf
Mathieu PILLET <math.pillet@hotmail.com>
2018-07-10 10:45:31 +0200

    Added sway files to sync.sh

--- sync.sh
+++ sync.sh
@@ -1,6 +1,9 @@
 # Just a little script to sync this folder with system dotfiles
 
-# zsh dotfiles
+# zsh
 cp -fv ./zshenv ~/.zshenv
 cp -fv ./config/zsh/zprofile ~/.config/zsh/.zprofile
 cp -fv ./config/zsh/zshrc ~/.config/zsh/.zshrc
+
+# sway
+cp -fv ./config/sway/* ~/.config/sway/
