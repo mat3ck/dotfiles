5d60a2087eee6404e309a83462a3359ac7afea5a
Mathieu PILLET <math.pillet@hotmail.com>
2018-07-11 13:19:34 +0200

    Fixed zsh sync in sync.sh

--- sync.sh
+++ sync.sh
@@ -3,8 +3,8 @@
 # zsh
 cp -fv ./zshenv ~/.zshenv
 cp -frv ./config/zsh ~/.config
-mv -fv ./config/zsh/zprofile ~/.config/zsh/.zprofile
-mv -fv ./config/zsh/zshrc ~/.config/zsh/.zshrc
+mv -fv ~/.config/zsh/zprofile ~/.config/zsh/.zprofile
+mv -fv ~/.config/zsh/zshrc ~/.config/zsh/.zshrc
 
 # sway
 cp -frv ./config/sway ~/.config
