c563cd7333ca14bcd5d5952bbaa730de28ec5c3f
Mathieu PILLET <math.pillet@hotmail.com>
2020-04-18 23:06:20 -0700

    Add zathura theming

--- sync.sh
+++ sync.sh
@@ -1,11 +1,26 @@
 ### Just a little script to sync this folder with system dotfiles
 
+# udev
+sudo cp -fr ./src/udev /etc
+
+# yay
+cp -fr ./src/yay ~/.config
+
 # zsh
 mkdir -p ~/.config/zsh
 cp -f ./src/zsh/zshenv -T ~/.zshenv
 cp -f ./src/zsh/zshrc -T ~/.config/zsh/.zshrc
 cp -f ./src/zsh/zprofile -T ~/.config/zsh/.zprofile
 cp -f ./src/zsh/p10k.zsh -T ~/.config/zsh/.p10k.zsh
+
+# ranger
+cp -fr ./src/ranger ~/.config
+
+# neovim
+cp -fr ./src/nvim ~/.config
+
+# git
+cp -f ./src/git/gitconfig -T ~/.gitconfig	
 
 # xorg
 mkdir -p ~/.config/xorg
@@ -16,8 +31,9 @@ sudo cp -f ./src/xorg/40-libinput.conf -T /etc/X11/xorg.conf.d/40-libinput.conf
 sudo cp -f ./src/xorg/20-modesetting.conf -T \
 	/etc/X11/xorg.conf.d/20-modesetting.conf
 
-# yay
-cp -fr ./src/yay ~/.config
+# xdg
+cp -f ./src/xdg/user-dirs.dirs -T ~/.config/user-dirs.dirs
+cp -f ./src/xdg/user-dirs.locale -T ~/.config/user-dirs.locale
 
 # awesome
 cp -fr ./src/awesome ~/.config
@@ -34,21 +50,9 @@ cp -fr ./src/polybar ~/.config
 # termite
 cp -fr ./src/termite ~/.config
 
-# ranger
-cp -fr ./src/ranger ~/.config
-
 # rofi
 cp -fr ./src/rofi ~/.config
 
-# neovim
-cp -fr ./src/nvim ~/.config
+# zathura
+cp -fr ./src/zathura ~/.config
 
-# xdg
-cp -f ./src/xdg/user-dirs.dirs -T ~/.config/user-dirs.dirs
-cp -f ./src/xdg/user-dirs.locale -T ~/.config/user-dirs.locale
-
-# git
-cp -f ./src/git/gitconfig -T ~/.gitconfig	
-
-# udev
-sudo cp -fr ./src/udev /etc
