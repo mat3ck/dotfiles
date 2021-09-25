158896800eb924a26b3e4f793abe33acf9bc97c8
Mathieu PILLET <math.pillet@hotmail.com>
2021-09-25 18:56:24 +0200

    quick reconfig for i3

--- src/polybar/launch.sh
+++ src/polybar/launch.sh
@@ -4,9 +4,21 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null
+do
+    sleep 1
+done
 
 # Launch main bar
-polybar main
+if type "xrandr"
+then
+    for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1)
+    do
+        MONITOR=$monitor polybar --reload main &
+    done
+else
+    polybar --reload main &
+fi
 
-echo "Bar launched..."
+echo "polybar launched"
+
